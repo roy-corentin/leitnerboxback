3b8b2186c29f9eb9ede106c7547cfe3da48c91bc
roy-corentin <corentin.roy@epitech.eu>
2023-11-11 21:42:41 +0100

    [ADD] new attribute to SaveDeck

--- src/operations/save_deck.cr
+++ src/operations/save_deck.cr
@@ -1,9 +1,10 @@
 class SaveDeck < Deck::SaveOperation
   include ValidateObjectBelongsToUser
+
   param_key :deck
 
   attribute user_id : Int64
-  permit_columns leitner_box_id, period_unit, period_type
+  permit_columns leitner_box_id, period_unit, period_type, level
 
   before_save validate_box_belongs_to_user
 end
