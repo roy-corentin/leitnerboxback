c33a838445b386dc7dde99320c4177fe2f45091b
roy-corentin <corentin.roy@epitech.eu>
2023-11-13 10:08:37 +0100

    [ADD] extract queries

--- src/operations/move_card_according_to_review.cr
+++ src/operations/move_card_according_to_review.cr
@@ -25,7 +25,7 @@ class MoveCardAccordingToReview < Avram::Operation
   def move_card_to_upper_deck
     card = CardQuery.find(card_id.value.not_nil!)
     deck = DeckQuery.find(deck_id.value.not_nil!)
-    upper_deck = DeckQuery.new.leitner_box_id(leitner_box_id.value.not_nil!).level.gt(deck.level).first?
+    upper_deck = DeckQuery.new.upper_deck?(leitner_box_id.value.not_nil!, deck.level)
     if upper_deck
       SaveCard.update!(card, deck_id: upper_deck.id, leitner_box_id: upper_deck.leitner_box_id, user_id: user_id.value.not_nil!)
     end
@@ -34,7 +34,7 @@ class MoveCardAccordingToReview < Avram::Operation
   def move_card_to_lower_deck
     card = CardQuery.find(card_id.value.not_nil!)
     deck = DeckQuery.find(deck_id.value.not_nil!)
-    lower_deck = DeckQuery.new.leitner_box_id(leitner_box_id.value.not_nil!).level.desc_order.level.lt(deck.level).first?
+    lower_deck = DeckQuery.new.lower_deck?(leitner_box_id.value.not_nil!, deck.level)
     if lower_deck
       SaveCard.update!(card, deck_id: lower_deck.id, leitner_box_id: lower_deck.leitner_box_id, user_id: user_id.value.not_nil!)
     end
--- src/queries/deck_query.cr
+++ src/queries/deck_query.cr
@@ -1,2 +1,10 @@
 class DeckQuery < Deck::BaseQuery
+  def upper_deck?(leitner_box_id, start_level)
+    self.leitner_box_id(leitner_box_id).level.gt(start_level).first?
+  end
+
+  def lower_deck?(leitner_box_id, start_level)
+    decks_level_desc_order = self.level.desc_order
+    decks_level_desc_order.leitner_box_id(leitner_box_id).level.lt(start_level).first?
+  end
 end
