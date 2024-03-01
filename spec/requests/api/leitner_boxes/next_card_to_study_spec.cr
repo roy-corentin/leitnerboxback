5d364bfea99fbaa17f1b85f5498081344fcca759
roy-corentin <corentin.roy02@laposte.net>
2024-03-01 22:35:53 +0100

    [ADD] spec next_card_to_study operation

--- spec/requests/api/leitner_boxes/next_card_to_study_spec.cr
+++ spec/requests/api/leitner_boxes/next_card_to_study_spec.cr
@@ -3,15 +3,30 @@ require "../../../spec_helper"
 describe Api::LeitnerBoxes::NextCardToStudy do
   describe "user authenticated" do
     describe "with cards to study" do
-      it "returns the card to study" do
-        user = UserFactory.create
-        leitner_box = LeitnerBoxFactory.create &.user_id(user.id)
-        deck = DeckFactory.create &.leitner_box_id(leitner_box.id)
-        card = CardFactory.create &.deck_id(deck.id)
+      describe "with last_review_at set to nil" do
+        it "returns the card to study" do
+          user = UserFactory.create
+          leitner_box = LeitnerBoxFactory.create &.user_id(user.id)
+          deck = DeckFactory.create &.leitner_box_id(leitner_box.id)
+          card = CardFactory.create &.deck_id(deck.id)
 
-        response = ApiClient.auth(user).exec(Api::LeitnerBoxes::NextCardToStudy.with(leitner_box.id))
+          response = ApiClient.auth(user).exec(Api::LeitnerBoxes::NextCardToStudy.with(leitner_box.id))
 
-        response.should send_json(200, id: card.id)
+          response.should send_json(200, id: card.id)
+        end
+      end
+
+      describe "with last_review_at set above period" do
+        it "returns ok" do
+          user = UserFactory.create
+          leitner_box = LeitnerBoxFactory.create &.user_id(user.id)
+          deck = DeckFactory.create &.leitner_box_id(leitner_box.id)
+          card = CardFactory.create &.deck_id(deck.id).last_review_at(Time.utc - 2.week)
+
+          response = ApiClient.auth(user).exec(Api::LeitnerBoxes::NextCardToStudy.with(leitner_box.id))
+
+          response.should send_json(200, id: card.id)
+        end
       end
     end
 
@@ -20,7 +35,7 @@ describe Api::LeitnerBoxes::NextCardToStudy do
         user = UserFactory.create
         leitner_box = LeitnerBoxFactory.create &.user_id(user.id)
         deck = DeckFactory.create &.leitner_box_id(leitner_box.id)
-        card = CardFactory.create &.deck_id(deck.id).last_review_at(Time.utc.shift(months: 1))
+        card = CardFactory.create &.deck_id(deck.id).last_review_at(Time.utc)
 
         response = ApiClient.auth(user).exec(Api::LeitnerBoxes::NextCardToStudy.with(leitner_box.id))
 
