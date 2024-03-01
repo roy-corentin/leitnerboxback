c34dee97b0b6b2b996fa3e50e2c3e8a66e2d5eb8
roy-corentin <corentin.roy02@laposte.net>
2024-03-01 22:21:07 +0100

    [UP] improve query to get next card to study

--- src/models/deck.cr
+++ src/models/deck.cr
@@ -19,19 +19,16 @@ class Deck < BaseModel
     return nil unless self.need_review?
 
     current_date = Time.utc
-    CardQuery.new.deck_id(self.id).each do |card|
-      if card.last_review_at.nil? || card.last_review_at.not_nil! + period < current_date
-        return card
-      end
-    end
-
-    nil
+    CardQuery.new.deck_id(self.id)
+      .last_review_at.is_nil
+      .or(&.last_review_at.lte(current_date - period))
+      .first?
   end
 
   def need_review?
     return true if last_review_at.nil?
 
-    # TODO remove 'not_nil!' when Crystal compile-time type check fixed
+    # TODO remove 'not_nil!' when Lucky compile-time type check fixed
     next_review_date = last_review_at.not_nil! + period
     current_date = Time.utc
 
