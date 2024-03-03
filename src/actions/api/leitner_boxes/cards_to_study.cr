691d931ee5ef99d914fdad3bad6e73d9fb60d307
roy-corentin <corentin.roy02@laposte.net>
2024-03-03 19:19:13 +0100

    [UP] endpoint url cards_to_study

--- src/actions/api/leitner_boxes/cards_to_study.cr
+++ src/actions/api/leitner_boxes/cards_to_study.cr
@@ -1,5 +1,5 @@
 class Api::LeitnerBoxes::CardsToStudy < ApiAction
-  get "/leitner_boxes/:leitner_box_id/next_card_to_study" do
+  get "/leitner_boxes/:leitner_box_id/cards_to_study" do
     FetchCardsToStudy.run(user_id: current_user.id, leitner_box_id: leitner_box_id.to_i) do |operation, cards_to_study|
       if cards_to_study && cards_to_study.size > 0
         json({cards: cards_to_study.map { |card| CardSerializer.new(card) }})
